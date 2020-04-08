dcdc30e40edea414e1e57f03f803057af9b518c9
Albert <zuendorf@uni-kassel.de>
2020-04-08 20:21:29 +0200

    still card to cart

--- test/src/main/java/unikassel/websystem/Shop/AddToCart.java
+++ test/src/main/java/unikassel/websystem/Shop/AddToCart.java
@@ -2,7 +2,7 @@ package unikassel.websystem.Shop;
 import java.beans.PropertyChangeSupport;
 import java.beans.PropertyChangeListener;
 
-public class AddToCart extends ModelCommand
+public class AddToCart extends ModelCommand  
 {
    @Override
    public Object run(ShopEditor modelEditor)
