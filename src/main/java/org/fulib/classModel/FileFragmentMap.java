8f5c5b3375253a6ab51a7fbae891c06c43b9d096
Albert <zuendorf@uni-kassel.de>
2020-08-18 19:59:40 +0200

    Fix for FileFragmentMap now having only one attribute part after parsing

--- src/main/java/org/fulib/classModel/FileFragmentMap.java
+++ src/main/java/org/fulib/classModel/FileFragmentMap.java
@@ -291,7 +291,13 @@ public class FileFragmentMap
             continue;
          }
 
-         final Fragment lastChild = children.get(children.size() - 1);
+         Fragment lastChild = children.get(children.size() - 1);
+         for (Fragment child : children) {
+            if (subKey.equals(child.getKey())) {
+               lastChild = child;
+               break;
+            }
+         }
          if (!subKey.equals(lastChild.getKey()))
          {
             next = new CompoundFragment().setKey(subKey).setParent(next);
