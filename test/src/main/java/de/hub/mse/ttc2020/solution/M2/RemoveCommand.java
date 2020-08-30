f9bb607d5ed8ec428154c3559963f289986f414f
Albert <zuendorf@uni-kassel.de>
2020-08-30 11:58:04 +0200

    RemoveCommand refactored

--- test/src/main/java/de/hub/mse/ttc2020/solution/M2/RemoveCommand.java
+++ test/src/main/java/de/hub/mse/ttc2020/solution/M2/RemoveCommand.java
@@ -60,19 +60,11 @@ public class RemoveCommand extends ModelCommand
 
    public Object run(M2Editor editor)
    {
-      java.util.Map<String,Object> mapOfModelObjects = editor.getMapOfModelObjects();
-      java.util.Map<String,Object> mapOfFrames = editor.getMapOfFrames();
-
-      Object oldObject = mapOfModelObjects.remove(getId());
-
-      if (oldObject != null) {
-         mapOfFrames.put(getId(), oldObject);
-      }
-
-      // call undo on old command
+      editor.removeModelObject(getId());
       ModelCommand oldCommand = editor.getActiveCommands().get(getId());
-      oldCommand.undo(editor);
-
+      if (oldCommand != null) {
+         oldCommand.undo(editor);
+      }
       return null;
    }
 
